0a705b8dde2fe8b8291d0b16ab9a6c398b9b4a46
Evaldas88 <e.skackauskas@gmail.com>
2022-12-12 09:01:09 +0200

    created router , header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+
+const Header = () => {
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        const role = localStorage.getItem('user_role')
+
+        if (token && role) {
+            setUser({
+                loggedIn: true,
+                token,
+                role
+            })
+        }
+    }, [])
+
+    return (
+        <header className="navbar-dark bg-dark">
+            <div className="container d-flex justify-content-between">
+                <div className=" "></div>
+                <nav>
+                    <ul >
+                        <li ><Link className='text-white h4 text-center' to="/">Home</Link></li>
+                        {!user.loggedIn && (
+                            <>
+                                <li><Link className='text-white h4' to="/login">Login</Link></li>
+                                <li><Link className='text-white h4' to="/register">Registration</Link></li>
+                            </>
+                        )}
+                        {user.loggedIn && (
+                            <>
+                                <li><Link className='text-white h4' to="/applications">Applications</Link></li>
+                                <li><Link className='text-white h4' to="/logout">Logout</Link></li>
+                            </>
+                        )}
+                        {user.loggedIn && user.role === '0' && (
+                            <>
+                                <li>
+                                    <Link className='text-white h4' to="/admin/applications">Admin</Link>
+                                    <ul class="dropdown-menu">
+                                        <li><Link className='h5 text-white' to="/admin/applications">Applications</Link></li>
+                                        <li><Link className='h5  text-white' to="/admin/schools">Schools</Link></li>
+                                    </ul>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/router/Router.js
@@ -0,0 +1,70 @@
+import '../App.css';
+import { useEffect, useState } from 'react'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Logout from '../pages/Logout';
+import AdminApplications from '../pages/admin/Applications';
+import Schools from '../pages/admin/schools/Schools';
+import EditShool from '../pages/admin/schools/EditSchool';
+import NewSchool from '../pages/admin/schools/NewSchool';
+import Applications from '../pages/Applications';
+import StudentApplication from '../pages/loggin/Orders';
+
+
+
+const App = () => {
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+      const token = localStorage.getItem('token')
+      const role = localStorage.getItem('user_role')
+
+      if(token && role) {
+          setUser({
+              loggedIn: true,
+              token,
+              role
+          })
+      }
+  }, [])
+
+  const logoutUser = () => {
+      setUser({
+          loggedIn: false
+      })
+  }
+
+  return (
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              {!user.loggedIn && <Route path="/login" element={<Login setUser={setUser}/>} /> }
+              {!user.loggedIn && <Route path="/register" element={<Register />} /> } 
+              {user.loggedIn && user.role == '0' && (
+                    <>
+                        <Route path="/admin/applications" element={<AdminApplications />} /> 
+                        <Route path="/admin/schools" element={<Schools />} /> 
+                        <Route path="/admin/schools/new" element={<NewSchool />} /> 
+                        <Route path="/admin/schools/edit/:id" element={<EditShool />} /> 
+                    </>
+                )}
+                {user.loggedIn && (
+                    <>
+                        <Route path="/applications" element={<Applications/>} /> 
+                        <Route path="/student/order" element={<StudentApplication/>} /> 
+                    </>
+                )}
+              <Route path="/logout" element={<Logout logoutUser={logoutUser} />} /> 
+
+          </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
