c31abad22aef9f026ffb7d8d36d866c8df8cb63d
Evaldas88 <e.skackauskas@gmail.com>
2022-12-20 14:21:34 +0200

    fixed registration style

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -22,61 +22,65 @@ const Register = () => {
     useEffect(() => {
         setLoading(false)
         const token = localStorage.getItem('token')
-        if(token) {
+        if (token) {
             navigate('/')
-        } 
+        }
     }, [navigate])
 
     const handleFormChange = (e) => {
-        setRegisterForm({...registerForm, [e.target.name]: e.target.value})
+        setRegisterForm({ ...registerForm, [e.target.name]: e.target.value })
     }
 
     const handleFormSubmit = (e) => {
         e.preventDefault()
         setLoading(true)
         axios.post('http://127.0.0.1:8000/api/register', registerForm)
-        .then(resp => {
-            if(resp.status === 200) {
-                localStorage.setItem('token', resp.data.message.token)
-                localStorage.setItem('user_role', resp.data.message.role)
+            .then(resp => {
+                if (resp.status === 200) {
+                    localStorage.setItem('token', resp.data.message.token)
+                    localStorage.setItem('user_role', resp.data.message.role)
+                    setLoading(false)
+                    navigate('/')
+                }
+            })
+            .catch(err => {
                 setLoading(false)
-                navigate('/')
-            }
-        })
-        .catch(err => {
-            setLoading(false)
-            if(err.response.data)
-                setMessage({text: err.response.data.message, status: 'danger'})
-            else 
-                setMessage({text: 'The server is dead', status: 'danger'})
-        })
+                if (err.response.data)
+                    setMessage({ text: err.response.data.message, status: 'danger' })
+                else
+                    setMessage({ text: 'The server is dead', status: 'danger' })
+            })
     }
- 
+
     return (
         <>
-            {loading && ( <div className="loading">Loading...</div> )}
+            {loading && (<div className="loading">Loading...</div>)}
             <Header />
-            <main className="text-center registerForm">
-                <div className="form-signin w-100 m-auto">
-                    <h1 className="h2 mb-3 fw-normal text-light">Registration</h1>
-                    <Message value={message} />
-                    <form onSubmit={handleFormSubmit}>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control" name="name" onChange={handleFormChange} placeholder="Jūsų vardas" value={registerForm.name} />
-                            <label >Name</label>
+            <div className="d-flex aligns-items-center justify-content-center">
+                <div className="card w-25">
+                    <div className="card-header ">Registration
+
+                        <div className="card-body">
+                            <Message value={message} />
+                            <form onSubmit={handleFormSubmit}>
+                                <div className="form-label mt-2">
+                                    <label >Name</label>
+                                    <input type="text" className="form-control mt-1" name="name" onChange={handleFormChange} placeholder="Your name" value={registerForm.name} />
+                                </div>
+                                <div className="form-label mt-2">
+                                    <label>Email</label>
+                                    <input type="email" className="form-control mt-1" name="email" onChange={handleFormChange} placeholder="name@example.com" value={registerForm.email} />
+                                </div>
+                                <div className="form-label mt-2">
+                                    <label>Password</label>
+                                    <input type="password" className="form-control mt-1" name="password" onChange={handleFormChange} placeholder="Password" value={registerForm.password} />
+                                </div>
+                                <button className="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
+                            </form>
                         </div>
-                        <div className="form-floating mb-3">
-                            <input type="email" className="form-control" name="email" onChange={handleFormChange} placeholder="name@example.com" value={registerForm.email} />
-                            <label>Email</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="password" className="form-control" name="password" onChange={handleFormChange} placeholder="Slaptažodis" value={registerForm.password} />
-                            <label>Password</label>
-                        </div>
-                        <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
-                    </form> 
+                    </div>
                 </div>
-            </main>
+            </div>
             {/* <Footer/> */}
         </>
     )
